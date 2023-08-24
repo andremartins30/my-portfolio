3a4c70360f8cfa4c87081fe658169ab09c424129
andremartins30 <87194533+andremartins30@users.noreply.github.com>
2023-08-23 20:12:43 -0400

    Ajuste app.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Mude de Switch para Routes
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Contact from './components/Contact/Contact';
 import './App.css'
-import './components/Header/Header.css'
 import ProjectsSection from './components/projectsSection/ProjectsSection';
 const App = () => {
   return (
