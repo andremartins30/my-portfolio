98ed1bd4a2376d026a6e91b2b735ea4fd689e157
andremartins30 <87194533+andremartins30@users.noreply.github.com>
2023-09-12 21:47:17 -0400

    update about

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Contact from './components/Contact/Contact';
-import About from './components/About/about';
+import About from './components/About/About';
 import './App.css'
 import ProjectsSection from './components/projectsSection/ProjectsSection';
 const App = () => {
